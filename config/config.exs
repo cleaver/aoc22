2523fe31739ee2702975c293f3886f263e3f95b2
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-09-30 13:39:41 -0400

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :advent_of_code_utils,
+  session:
+    "53616c7465645f5f3911464ace4d9e8889afda85c852e720591f3e66c38df3b61daadebca6e2449c30748040a877c49ef3006ad2c8b8356b2463f19323140aaf",
+  auto_compile?: true,
+  time_calls?: true,
+  gen_tests?: true
