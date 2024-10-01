d9ab880f4bcb7470fd6ce6c5996397039621cb92
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-10-01 11:06:27 -0400

    day1

--- config/config.exs
+++ config/config.exs
@@ -5,4 +5,6 @@ config :advent_of_code_utils,
     "53616c7465645f5f3911464ace4d9e8889afda85c852e720591f3e66c38df3b61daadebca6e2449c30748040a877c49ef3006ad2c8b8356b2463f19323140aaf",
   auto_compile?: true,
   time_calls?: true,
-  gen_tests?: true
+  gen_tests?: true,
+  year: 2022,
+  day: 1
--- lib/2022/1.ex
+++ lib/2022/1.ex
@@ -1,9 +1,34 @@
 import AOC
+import InputHelpers
 
 aoc 2022, 1 do
   def p1(input) do
+    process_input(input)
+    |> Enum.max()
   end
 
   def p2(input) do
+    process_input(input)
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.slice(0..2)
+    |> Enum.sum()
+  end
+
+  defp process_input(input) do
+    input
+    |> parse_input(false)
+    |> IO.inspect(label: "input")
+    |> Enum.reduce([[]], fn calories, [current | history] ->
+      case Integer.parse(calories) do
+        {num_calories, _} ->
+          [[num_calories | current] | history]
+
+        :error ->
+          [[], current | history]
+      end
+    end)
+    |> Enum.map(fn calories_list ->
+      Enum.sum(calories_list)
+    end)
   end
 end
